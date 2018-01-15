651cc8c64682075c3b636531e3f1dd09d7bfe80a
btorres-miller.greenriverdev.com <btorresm@srv603.webhostingforstudents.com>
2018-01-15 04:57:46 -0500

    Adding file

---
+++ arrays_Functions.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * Bessy Torres-Miller
+ * IT-328
+ * Functions and Arrays PHP review
+ */
+
+//Error reporting
+ini_set('display_error' ,1);
+error_reporting(E_ALL);
+
+//PART I----------------------------------------------------------------------------------------------------------------
+echo "<H3>Part I</H3>";
+
+//1.Define an array
+$animals = array("panda", "alpaca", "boa");
+
